603a35a620f9034d719aab11250932043648132d
Jonathan Lee <joncslee@gmail.com>
2013-02-13 00:48:40 -0500

    added comments, article layout, refined header style, stubbed footer

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ActiveRecord::Base
   attr_accessible :content, :title, :article_type, :tags, :format
+
+  def next
+    Article.find(:first, :conditions => ["id > ?", self.id])
+  end
+
+  def previous
+    Article.find(:first, :conditions => ["id < ?", self.id])
+  end
+
 end
